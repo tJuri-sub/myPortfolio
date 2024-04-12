3148cec4e93d6b708a20a7f939a8afb9f5efd4f7
Juri Turiano <juricuber@gmail.com>
2024-04-12 21:42:58 +0800

    added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ const HiCard = () => {
       <h1 className="text-portfolio-secondary text-4xl py-4 px-6 rounded-lg border-portfolio-stroke/40 border bg-secondary-black bg-opacity-40 animate-bounce">
         HI!
       </h1>
-      <p className="text-portfolio-secondary text-center text-3xl mt-5">
+      <p className="bg-gradient-to-r from-portfolio-accent to-portfolio-highlight bg-clip-text text-transparent text-center text-3xl mt-5">
         Lets Build Creativity with fun!
       </p>
     </div>
